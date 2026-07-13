371b42335467019a1f200b1ed4164a74d9846271
jscaff <jscaffidi@qnx.com>
2026-07-13 17:49:33 +0000

    change copyright header for the standard blackberry one

--- qnx/QNXMachine.h
+++ qnx/QNXMachine.h
@@ -3,7 +3,7 @@
 /*
 htop - qnx/QNXMachine.h
 (C) 2024 htop dev team
-(C) 2026 QNXe team
+Copyright (c) 2026, BlackBerry Limited. All rights reserved.
 Released under the GNU GPLv2+, see the COPYING file
 in the source distribution for its full text.
 */
